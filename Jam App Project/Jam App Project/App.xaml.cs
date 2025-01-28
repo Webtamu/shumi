7a11836180899ba41a96436dcdc160baa2cccad7
Matthew Merioles <matthewmerioles@yahoo.com>
2025-01-27 16:40:19 -0800

    1/27 : First UI component test

--- Jam App Project/Jam App Project/App.xaml.cs
+++ Jam App Project/Jam App Project/App.xaml.cs
@@ -8,9 +8,6 @@ using System.Windows;
 
 namespace Jam_App_Project
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
