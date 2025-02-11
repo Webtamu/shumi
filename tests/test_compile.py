9220ab682568e6ea91e6ee111a658263074be779
Matthew Merioles <matthewmerioles@yahoo.com>
2025-02-10 22:38:57 -0800

    Compile test

---
+++ tests/test_compile.py
@@ -0,0 +1,8 @@
+import pytest
+import py_compile
+
+def test_main_compiles():
+    try:
+        py_compile.compile("main.py", doraise=True)
+    except py_compile.PyCompileError:
+        pytest.fail("main.py failed to compile")
